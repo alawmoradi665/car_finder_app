a8a89778f28b16109a955e8811cf345373e69032
alawmoradi665 <alaw.m@hotmail.com>
2021-07-12 12:51:00 +1000

    add images function

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,6 @@
 class Listing < ApplicationRecord
   # belongs_to :profile
   belongs_to :buyer, class_name: "Profile", optional: true 
-  benlongs_to :seller, class_name: "Profile"
-
+  belongs_to :seller, class_name: "Profile"
+  has_many_attached :pictures
 end
